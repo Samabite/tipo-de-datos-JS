cfc13cf0f7ac514fc2c7bdf0318da5895576cdfa
Samaelbite <samwiliamh12@gmail.com>
2025-02-10 15:58:18 -0600

    se modifico el archivo JS

--- mains.js
+++ mains.js
@@ -1,2 +1,44 @@
-// Este es mi primer script
-console.log("Hola");
+let numero = 42;
+
+console.log(typeof numero);
+
+
+let string = "veinticinco";
+
+console.log(typeof string);
+
+let seis = -666;
+
+console.log(typeof seis);
+
+
+let truth = true; 
+
+console.log(typeof truth);
+
+let cero = 0; 
+
+console.log(typeof cero);
+
+let comillas = "";
+
+console.log(typeof comillas);
+
+let nulo = null;
+
+console.log(typeof nulo);
+
+let unde = undefined;
+
+console.log(typeof unde);
+
+let falso = false;
+
+console.log(typeof falso);
+
+
+let suma = 5 + "5";
+
+console.log(suma);
+
+
